790b334fd95d304092bbeea4637dc40b4da94721
Mark Tiddy <marktiddy@gmail.com>
2020-04-13 14:59:48 +0100

    updated dist

--- main.js
+++ main.js
@@ -3,14 +3,26 @@ AOS.init();
 $(document).ready(() => {
   const closePlayer = () => {
     $("#music-player").fadeOut();
-    $("#show-music-player").fadeIn();
+    setTimeout(() => {
+      $("#show-music-player").fadeIn();
+    }, 500);
   };
 
   const showPlayer = () => {
     $("#show-music-player").fadeOut();
-    $("#music-player").fadeIn();
+    setTimeout(() => {
+      $("#music-player").fadeIn();
+    }, 500);
   };
 
   $("#close-button").click(() => closePlayer());
   $("#show-player").click(() => showPlayer());
+
+  //Stop vertical scrolling
+});
+var $body = $(document);
+$body.bind("scroll", () => {
+  if ($body.scrollLeft() !== 0) {
+    $body.scrollLeft(0);
+  }
 });
